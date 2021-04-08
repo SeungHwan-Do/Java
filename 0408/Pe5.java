3db651d2664a44808a79a21e3135bc3279a09d30
SeungHwan-Do <79446573+SeungHwan-Do@users.noreply.github.com>
2021-04-08 17:44:37 +0900

    Update Pe5.java

--- 0408/Pe5.java
+++ 0408/Pe5.java
@@ -1,24 +1,38 @@
 class Account{
-	int balance=0;
+	
+	int balance = 0;
+	
+	int id = 0;
+	static int cnt = 1;
+	
+	public Account() {
+		System.out.println("새로운 계좌가 만들어졌습니다.");
+		id = cnt++;
+	}
+	
 	void withdraw(int amount) {
 		balance -= amount;
-		System.out.print(amount+"원 인출");
+		System.out.println(amount+"원 인출");
 	}
 	void deposit(int amount) {
 		balance += amount;
-		System.out.print(amount+"원 저축");}
-	void account() {System.out.print(balance);
+		System.out.println(amount+"원 저축");
 	}
-	void Account() {System.out.print("새로운 계좌가 만들어졌습니다.");}
+	void print() {
+		System.out.println("고객 #" +id + " 계좌 잔고=" + balance);
+	}
 	
 }
-public class Pe5 {
 
+public class Pe5 {
 	public static void main(String[] args) {
 		Account acc1 = new Account();
-		Account acc2 = new Account();
 		acc1.deposit(50000);
+		Account acc2 = new Account();
 		acc2.deposit(100000);
+		
+		acc1.print();
+		acc2.print();
 	}
 
 }
